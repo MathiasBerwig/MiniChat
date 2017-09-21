0608751caf8bac31b450b80de87a26da5f6cec3d
Mathias Berwig <mathiasberwig@gmail.com>
2017-09-21 16:05:07 -0300

    Commit inicial

---
+++ app/src/main/java/io/github/mathiasberwig/minichat/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package io.github.mathiasberwig.minichat.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.mathiasberwig.minichat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
