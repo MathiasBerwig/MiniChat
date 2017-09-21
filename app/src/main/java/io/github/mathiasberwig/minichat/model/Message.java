06aac3742fcf276d9cde7a98d6c7f0a78f531d27
Mathias Berwig <mathiasberwig@gmail.com>
2017-09-21 17:12:13 -0300

    Classe Message adicionada

---
+++ app/src/main/java/io/github/mathiasberwig/minichat/model/Message.java
@@ -0,0 +1,48 @@
+package io.github.mathiasberwig.minichat.model;
+
+/**
+ * Created by mathias.berwig on 21/09/2017.
+ */
+
+public class Message {
+
+    private String id;
+    private String text;
+    private String name;
+    private String photoUrl;
+
+    public Message() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
